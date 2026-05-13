67091962f5d7a0871129fc8adbad25c6f19b757d
kishore agilan <praveen271003@gmail.com>
2026-05-13 14:52:17 +0530

    BUILD

--- vite.config.js
+++ vite.config.js
@@ -16,15 +16,6 @@ export default defineConfig({
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor-react';
-            if (id.includes('lucide')) return 'vendor-icons';
-            if (id.includes('recharts') || id.includes('chart.js') || id.includes('d3')) return 'vendor-charts';
-            if (id.includes('tsparticles')) return 'vendor-particles';
-            return 'vendor';
-          }
-        }
       }
     }
   },
